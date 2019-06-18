08b5de656aca548c9c5dd963c9d97b2944da12e7
Ben <goodben12345@gmail.com>
2019-06-18 17:08:31 +0800

    Delete useless files

--- StrongTankEnemy.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef STRONGTANKENEMY_HPP
-#define STRONGTANKENEMY_HPP
-#include "Enemy.hpp"
-
-class StrongTankEnemy : public Enemy {
-public:
-	StrongTankEnemy(int x, int y);
-};
-#endif // STRONGTANKENEMY_HPP
